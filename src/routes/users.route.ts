c466ee743091eb677c367d14998d7350e6166f08
brenneralexandria <obrenneralexandria@gmail.com>
2023-02-01 14:30:59 -0300

    Desenvolvendo o update e o delete de usuários

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -21,17 +21,20 @@ usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction
     res.status(StatusCodes.CREATED).send(uuid);
 });
 
-usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction,) => {
+usersRoute.put('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction,) => {
     const uuid = req.params.uuid;
     const modifieUser = req.body;
 
     modifieUser.uuid = uuid;
 
-    res.status(StatusCodes.OK).send({ uuid });
+    await userRepository.uptade(modifieUser);
+
+    res.status(StatusCodes.OK).send();
 });
 
-usersRoute.delete('/users/:uuid', (req: Request, res: Response, next: NextFunction,) => {
+usersRoute.delete('/users/:uuid', async (req: Request, res: Response, next: NextFunction,) => {
     const uuid = req.params.uuid;
+    await userRepository.remove(uuid);
     res.sendStatus(StatusCodes.OK);
 });
 
